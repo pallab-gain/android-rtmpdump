29f59a0e9bf70eceafeb635cb95d46105b5d7d01
pallab-gain <pallab.gain@gmail.com>
2014-10-05 17:44:01 +0600

    finally just able to compile the c code with proper flags and linker library

---
+++ buildscript.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+NDK=/home/pallab/Documents/adt-bundle-linux-x86_64-20140702/ndk
+SYSROOT=$NDK/platforms/android-19/arch-arm
+TOOLCHAIN=$NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64
+OPENSSL_DIR=/home/pallab/Documents/Development/bd-tv/Try2/openssl-android
+
+
+ln -sf librtmp/librtmp.so.1 librtmp.so
+ln -s $SYSROOT/usr/lib/crtbegin_dynamic.o:
+ln -s $SYSROOT/usr/lib/crtend_android.o
+ln -s $SYSROOT/usr/lib/crtbegin_dynamic.o
+
+$TOOLCHAIN/bin/arm-linux-androideabi-gcc -Wall -marm -I$OPENSSL_DIR/include -isysroot $SYSROOT -I$SYSROOT/ -DRTMPDUMP_VERSION=\"v2.4\" -c -o rtmpdump.o rtmpdump.c
+$TOOLCHAIN/bin/arm-linux-androideabi-gcc -Wall  -L$OPENSSL_DIR/libs/armeabi -L$SYSROOT/usr/lib  -o rtmpdump rtmpdump.o -Llibrtmp -lrtmp -lssl -lcrypto -lz
--- readme.md
+++ readme.md
@@ -1,2 +1,20 @@
 One step at a time.... 
 Commits will be victorious  
+
+Build openssl for android :
+
+git clone https://github.com/guardianproject/openssl-android.git
+cd openssl-android
+
+#goto jni folder, commented out "NDK_TOOLCHAIN_VERSION=4.4.3" from Application.mk
+#now run
+ndk-build
+
+Build rtmpdump for android :
+
+git clone git://git.ffmpeg.org/rtmpdump
+copy librtmpbuildscript.sh into librtmp folder
+cd librtmp
+sh librtmpbuildscript.sh
+cd ..
+sh buildscript.sh
